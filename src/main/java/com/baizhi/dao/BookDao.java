32163870546c569f62ce1f38094c6789c86329bc
lihe <1815939234@qq.com>
2019-04-12 09:54:05 +0800

    v2.0

--- src/main/java/com/baizhi/dao/BookDao.java
+++ src/main/java/com/baizhi/dao/BookDao.java
@@ -1,7 +1,5 @@
 package com.baizhi.dao;
 
-import com.baizhi.entity.Book;
-
 import java.util.List;
 
 public interface BookDao {
--- src/main/java/com/baizhi/entity/Person.java
+++ src/main/java/com/baizhi/entity/Person.java
@@ -1,49 +1,55 @@
 package com.baizhi.entity;
 
-public class Book {
-    private Integer id;
-    private String name;
-    private String acthor;
+public class Person {
 
-    public Book() {
+
+        private Integer id;
+        private String name;
+        private String acthor;
+
+
+
+        public Integer getId() {
+            return id;
+        }
+
+        public void setId(Integer id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getActhor() {
+            return acthor;
+        }
+
+        public void setActhor(String acthor) {
+            this.acthor = acthor;
+        }
+
+
+    public Person() {
     }
 
-    public Book(Integer id, String name, String acthor) {
+    public Person(Integer id, String name, String acthor) {
         this.id = id;
         this.name = name;
-        this.acthor = acthor;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getActhor() {
-        return acthor;
-    }
-
-    public void setActhor(String acthor) {
         this.acthor = acthor;
     }
 
     @Override
     public String toString() {
-        return "Book{" +
+        return "Person{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
                 ", acthor='" + acthor + '\'' +
                 '}';
     }
 }
+
